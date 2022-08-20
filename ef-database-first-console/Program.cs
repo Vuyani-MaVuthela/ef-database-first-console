0e29f3fe7650cd982f5c2764ab8d13406977d3b8
Vuyani Moyo (MEA) <vuyani.moyo@dimensiondata.com>
2022-08-20 18:41:45 +0200

    first commit

---
+++ ef-database-first-console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ef_database_first_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DatabaseFirstDbEntities1();
+            var post = new Post()
+            { Body = "Body", DatePublished = DateTime.Now, Title = "Title", PostID = 1 };
+            context.Posts.Add(post);
+            context.SaveChanges();
+            Console.WriteLine("saved!");
+            Console.ReadKey();
+        }
+    }
+}
